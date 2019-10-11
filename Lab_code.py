5341e31c8563770c15ff461fa050f431bafe9fc3
Suren1987 <46677019+Suren1987@users.noreply.github.com>
2019-10-10 23:35:27 -0400

    Lab 7 codes

--- Lab_code.py
+++ Lab_code.py
@@ -1,6 +1,26 @@
-# Name: YOUR NAME HERE 
-# Assignment title: ENTER ASSIGNMENT TITLE HERE.
-# Time to complete: ENTER YOUR TIME HERE.
-# Description: ENTER A DESCRIIPTION ABOUT WHAT YOUR CODE IS SUPPOSED TO DO HERE.
+# Name: Surendra Shrestha
+# Assignment title: Refugee Stats with Dictionaries.
+# Time to complete: 30 minutes.
+# Description: This script contains three functions that derives three different outputs using the refugee 
+# data obtained from https://en.wikipedia.org/wiki/Refugee_camp. First, I created a dictionary 
+# with 5 key-value pairs consisting of a country name and an integer that represents the number of refugees
+# in refugee camps in five different countries, for year 2014. The first function prints out just the 
+# countries' name. The second function prints out just the population. The third function prints out a series
+# of statements stating the number of refugees for each country.
 
-Leave the lines of comment code, above, but add your personal details. Then replace these two lines of code with your new code! 
+Refugee_Country = {'Algeria': 90000, 'Botswana': 2833, 'Djibouti': 18208, 'India': 65057, 'Mauritania': 48910}
+
+def printDict(Refugee_Country):
+    for key in Refugee_Country.keys():
+        print key
+printDict(Refugee_Country)
+
+def printDict(Refugee_Country):
+    for value in Refugee_Country.values():
+        print value
+printDict(Refugee_Country)
+
+def printDict(Refugee_Country):
+    for (key, value) in Refugee_Country.items():
+        print key, "has", value, "refugees."
+printDict(Refugee_Country)
